1670a84f0d50dc18b0673b623ed9b6f0144db484
john <jericcarreon@yahoo.com>
2020-06-30 00:41:27 +0100

    Change IP and port and debug

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World ...again change Ip'
+    return 'Hello World ...again change Ip ME'
 
 
 if __name__ == '__main__':
     app.run()
+
 
 app.run(host=os.environ.get('IP', '0.0.0.0'),
         port=int(os.environ.get('PORT', '5000')),
