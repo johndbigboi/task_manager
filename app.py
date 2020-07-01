0f75cf988e14d8354fde29998198c2ba58cc3b18
john <jericcarreon@yahoo.com>
2020-07-01 23:39:08 +0100

    remove comments

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ if os.path.exists("env.py"):
     import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-# bson.objectid to convert the ID that's been passed across from our template into a readable format that's acceptable by MongoDB.
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
@@ -17,7 +16,6 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    # tasks collection, which will be returned from making a call directly to Mongo.
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
@@ -28,23 +26,19 @@ def add_task():
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks = mongo.db.tasks  # var to get db from mongo of tasks
-    tasks.insert_one(request.form.to_dict())  # send form to dictionary
-    # go to the task.html after sending form
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
 
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    # we're fetching the task that matches this task ID
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    # The second thing we need is a list of the collections because we're going to use the task that's returned from MongoDB and all the categories in order to populate a form for editing.
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task, categories=all_categories)
 
 
 @app.route('/update_task/<task_id>', methods=["POST"])
-# We pass in the task ID because that's our hook into the primary key.
 def update_task(task_id):
     tasks = mongo.db.tasks
     """ So what we do is we access the tasks collection.
@@ -70,13 +64,13 @@ def delete_task(task_id):
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
-                           categories=mongo.db.categories.find())  # to do a find on the categories table.
+                           categories=mongo.db.categories.find())
 
 
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
     category_doc = {'category_name': request.form.get(
-        'category_name')}  # send form to dictionary
+        'category_name')}
     mongo.db.categories.insert(category_doc)
     return redirect(url_for('get_categories'))
 
@@ -89,7 +83,7 @@ def add_category():
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
-                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))  # to do a find on the categories table.
+                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
 
 @app.route('/update_category/<category_id>', methods=["POST"])
